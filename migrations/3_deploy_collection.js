f51b83ed6e6b8f0f51588b5cd21277db51d47e84
nedodn <odennate@gmail.com>
2019-11-12 13:28:46 -0600

    update

--- migrations/3_deploy_collection.js
+++ migrations/3_deploy_collection.js
@@ -1,7 +1,7 @@
 var DexTradingWithCollection = artifacts.require("./DexTradingWithCollection.sol");
-var beneficiary = '0x5AEacDAaEc7C52e11799eFC756E6B89Cb3a32C3B';
-var dexag = '0xE3f5EA1e1212E02039Ec2dF87e567Ba1c79f3A03';
-var basisPoints = 250;
+var beneficiary = '';
+var dexag = '';
+var basisPoints = 0;
 
 module.exports = function(deployer) {
   deployer.deploy(DexTradingWithCollection, beneficiary, dexag, basisPoints);
